ff06cec3d685490247546cab5aa1f392fa7fecfb
Rafael Silverio <fael_st@Rafaels-MacBook-Air.local>
2021-12-05 16:34:49 -0300

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.userModel.find();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findOne({ _id: id });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: updateUserDto },
+      {
+        new: true,
+      },
+    );
+  }
+
+  remove(id: string) {
+    return this.userModel.findByIdAndRemove({ _id: id });
+  }
+}
